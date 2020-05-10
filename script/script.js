b8c6ad562dc6644d712a3481cad751bc46c1eecb
Natalya Myunster <natalyamyunster@yahoo.com>
2020-05-10 23:12:19 +0200

    2 functions: closeCart , openCart

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const search = document.querySelector('.search');
-     console.log('search: ', search);
-     console.dir('search');
+        //console.log('search: ', search);
+        //console.dir('search');
 
     const cartBtn = document.getElementById('cart');
     const wishlistBtn = document.getElementById('wishlist');
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
-        console.log(card);
+            //console.log(card);
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
         card.innerHTML = `<div class="card">
@@ -30,10 +30,30 @@ document.addEventListener('DOMContentLoaded', () => {
                             </div>
                         </div>`;
 
-        console.log(card);
+            //console.log(card);
         return card;
     };
 
-     goodsWrapper.appendChild(createCardGoods(1, 'Дартс', 2000, "./img/temp/Archer.jpg"));
-    goodsWrapper.appendChild(createCardGoods(1, 'Фламинго', 2000, "./img/temp/Flamingo.jpg"));
+    goodsWrapper.append(createCardGoods(1, 'Дартс', 2000, "./img/temp/Archer.jpg"));
+    goodsWrapper.append(createCardGoods(2, 'Фламинго', 2000, "./img/temp/Flamingo.jpg"));
+    goodsWrapper.append(createCardGoods(3,'Носки', 333, 'img/temp/Socks.jpg'));
+
+    const closeCart = (event) => {
+        const target = event.target;
+
+        if(target === cart || target.classList.contains('cart-close')){
+            cart.style.display = '';
+        }
+
+        console.log(target.classList.contains('cart-close'));
+        console.log(target.classList.contains('active'));
+
+    };
+
+    const openCart = () => {
+        cart.style.display = 'flex';
+    };
+
+    cartBtn.addEventListener('click', openCart);
+    cartBtn.addEventListener('click', closeCart);
 });
